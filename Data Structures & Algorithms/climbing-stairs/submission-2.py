1ef57374bc3ece1d3438e18392089c229c671a46
Cyrus Navasca <156489842+cyrusnavasca@users.noreply.github.com>
2026-08-28 00:41:14 -0700

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # cache
+        memo = {0: 1, 1: 1, 2: 2}
+        
+        # recursion
+        if n in memo:
+            return memo[n]
+        else:
+            memo[n] = (self.climbStairs(n-1) + self.climbStairs(n-2))
+            return memo[n]
+        
+        
