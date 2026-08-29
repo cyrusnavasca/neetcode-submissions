cb99709239f9e6ff7c7d788e9155ba20d23fb29d
Cyrus Navasca <156489842+cyrusnavasca@users.noreply.github.com>
2026-08-29 15:10:23 -0700

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # edge cases
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        
+        # initialize dp
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        # loop
+        for i in range(2, n):
+            curr_max = max((dp[i-2] + nums[i]), dp[i-1])
+            dp[i] = curr_max
+
+        return dp[-1]
+
+
+        
