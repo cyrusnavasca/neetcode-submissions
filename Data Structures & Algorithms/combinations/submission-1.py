a3b05b6ae489236684d9ff4a56039536e1ba3c49
Cyrus Navasca <156489842+cyrusnavasca@users.noreply.github.com>
2026-08-29 15:48:32 -0700

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = list(range(1, n+1))
+        res, sol = [], []
+
+        def backtrack(i):
+        # Q1: when is an answer complete?
+            if i >= n:
+                if len(sol) == k:
+                    res.append(sol[:])
+                return
+            
+        # Q2: what can we do at each state?
+            # dont pick i
+            backtrack(i+1)
+
+            # pick i
+            sol.append(nums[i])
+            backtrack(i+1)
+            sol.pop()
+        
+        backtrack(0)
+        return res
+        
+
+
+
+
+
+        
