a72313f35d11f3fd7d3221a0ae05c0a7966cedc0
Cyrus Navasca <156489842+cyrusnavasca@users.noreply.github.com>
2026-08-29 16:13:25 -0700

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res, sol = [], []
+
+        def backtrack(i):
+        # Q1: when is an answer complete
+            if len(sol) == n:
+                res.append(sol[:])
+                return
+
+        # Q2: what choices can we make at each state
+            for i in range(n):
+                if nums[i] not in sol:
+                    sol.append(nums[i])
+                    backtrack(i)
+                    sol.pop()
+
+        backtrack(0)
+        return res
