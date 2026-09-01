25f07509a6b5f3f25ff3e410a7e346f03c399c11
Cyrus Navasca <156489842+cyrusnavasca@users.noreply.github.com>
2026-09-01 00:05:17 -0700

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, len(dp)):
+            # set current target
+            target = i
+            current_minimum = math.inf
+
+            for coin in coins:
+                # if coin is bigger than target, skip
+                if coin > target: 
+                    break
+                diff = target - coin
+                coins_used = 1 + dp[diff]
+                current_minimum = min(current_minimum, coins_used)
+
+            dp[i] = current_minimum
+        
+        if dp[-1] == math.inf:
+            return -1
+        else:
+            return dp[-1]
+            
+                
